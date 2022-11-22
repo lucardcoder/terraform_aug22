88f8a313681b1d288ba2f5e0813e0a111e4df006
Your Name <you@example.com>
2022-11-22 02:09:32 +0000

    disabled alb

--- Class2/ASG/main.tf
+++ Class2/ASG/main.tf
@@ -25,47 +25,47 @@ module "asg" {
 }
 
 
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-  name = "my-alb"
-  load_balancer_type = "application"
-  # vpc_id             = "vpc-abcde012"
-  # subnets            = ["subnet-abcde012", "subnet-bcde012a"]
-  # security_groups    = ["sg-edcd9784", "sg-edcd9785"]
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "~> 8.0"
+#   name = "my-alb"
+#   load_balancer_type = "application"
+#   # vpc_id             = "vpc-abcde012"
+#   # subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+#   # security_groups    = ["sg-edcd9784", "sg-edcd9785"]
 
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
+#   access_logs = {
+#     bucket = "my-alb-logs"
+#   }
 
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "i-0123456789abcdefg"
-          port = 80
-        }
-        my_other_target = {
-          target_id = "i-a1b2c3d4e5f6g7h8i"
-          port = 8080
-        }
-      }
-    }
-  ]
+#   target_groups = [
+#     {
+#       name_prefix      = "pref-"
+#       backend_protocol = "HTTP"
+#       backend_port     = 80
+#       target_type      = "instance"
+#       targets = {
+#         my_target = {
+#           target_id = "i-0123456789abcdefg"
+#           port = 80
+#         }
+#         my_other_target = {
+#           target_id = "i-a1b2c3d4e5f6g7h8i"
+#           port = 8080
+#         }
+#       }
+#     }
+#   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+#   http_tcp_listeners = [
+#     {
+#       port               = 80
+#       protocol           = "HTTP"
+#       target_group_index = 0
+#     }
+#   ]
 
-}
+# }
 
 
 
