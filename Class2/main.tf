ddce687216131677abfbca79734ed0c5d6d8599c
Your Name <you@example.com>
2022-11-22 00:34:29 +0000

    added key pair

--- Class2/main.tf
+++ Class2/main.tf
@@ -1,4 +1,16 @@
+resource "aws_key_pair" "bastion-host" {
+  key_name   = "bastion-host-key-class2"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t3.micro"
+  associate_public_ip_address  = true
+  availability_zone  = "us-east-1a"
+  key_name = aws_key_pair.bastion-host.key_name
+  monitoring = true
+  tags = {
+    Name = "Test"
+  }
 }
