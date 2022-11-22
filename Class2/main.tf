8cc76bcc424617a232c8d23f87f5ccae9e099c0a
Your Name <you@example.com>
2022-11-22 00:39:18 +0000

    added sec group

--- Class2/main.tf
+++ Class2/main.tf
@@ -3,6 +3,28 @@ resource "aws_key_pair" "bastion-host" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-testing"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t3.micro"
@@ -10,6 +32,9 @@ resource "aws_instance" "web" {
   availability_zone  = "us-east-1a"
   key_name = aws_key_pair.bastion-host.key_name
   monitoring = true
+  vpc_security_group_ids = [
+    aws_security_group.allow_tls.id
+  ]
   tags = {
     Name = "Test"
   }
