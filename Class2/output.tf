ad6e4ae202e28895600fbb9cad5de6fb44fb0ca3
Your Name <you@example.com>
2022-11-22 00:55:54 +0000

    added more output

--- Class2/output.tf
+++ Class2/output.tf
@@ -13,3 +13,11 @@ output "id" {
 output "public_ip" {
   value = aws_instance.web.public_ip
 }
+
+output "instance_state" {
+  value = aws_instance.web.instance_state
+}
+
+output "public_dns" {
+  value = aws_instance.web.public_dns 
+}
