ccf307908c3260ba7ef87f649c14a277779fbad2
Your Name <you@example.com>
2022-11-24 01:50:26 +0000

    added random

--- Class3/SQL/main.tf
+++ Class3/SQL/main.tf
@@ -1,10 +1,23 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources-sql"
+  name     = "example-resources"
   location = "West Europe"
 }
 
+
+
+resource "random_string" "random" {
+length = 14
+special = false
+override_special = "/@£$"
+upper = false
+}
+
+
+
+
+
 resource "azurerm_mysql_server" "example" {
-  name                = "example-mysqlserver"
+  name                = "example-mysqlserver-${random_string.random.result}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
