6e0ad40fea165bbf8636f389ec432f60f0b40a71
Islam Kambarov <ikambarov@evolvecyber.com>
2022-11-29 00:55:38 +0000

    Update to use Variables

---
+++ Class5/variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  type        = string
+  description = "Enter region"
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Enter VPC Name"
+  default     = ""
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "Enter VPC CIDR Range"
+  default     = ""
+}
+
+variable "priv_subnets" {
+  type        = list(any)
+  description = "Enter Private Subnets"
+  default     = []
+}
+
+variable "pub_subnets" {
+  type        = list(any)
+  description = "Enter Private Subnets"
+  default     = []
+}
+
+
